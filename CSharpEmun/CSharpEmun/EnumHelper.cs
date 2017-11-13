28fe08a43f6e0cb49465cc353ae3f9ecef637d3c
H <hsl1991@live.com>
2017-11-13 23:01:17 +0800

    init

---
+++ CSharpEmun/CSharpEmun/EnumHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEmun
+{
+    public static class EnumHelper
+    {
+        public static string GetEnumName(this Enum thisEnum)
+        {
+            return Enum.GetName(thisEnum.GetType(), thisEnum);
+        }
+    }
+}
---
+++ CSharpEmun/CSharpEmun/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEmun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestEnum testEnum = TestEnum.A;
+            Console.WriteLine(testEnum.ToString());
+
+            Console.WriteLine(Enum.GetName(typeof(TestEnum), testEnum));
+            Console.WriteLine(testEnum.GetEnumName());
+            Array enumArray= Enum.GetValues(testEnum.GetType());
+            for (int i=0; i<enumArray.Length;i++)
+            {
+                Console.WriteLine("Name:{0},Value:{0:D}",enumArray.GetValue(i));
+            }
+            Console.ReadKey();
+
+        }
+    }
+
+    public enum TestEnum
+    {
+        A=1,
+        B=2,
+        C=4
+    }
+}
