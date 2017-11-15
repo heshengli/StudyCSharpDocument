20bb2c8a95c1d4982712e3d169c4edcc4cbc9a06
H <hsl1991@live.com>
2017-11-15 23:21:04 +0800

    ArrayList

---
+++ CSharpEmun/TestArrayList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList strList = new ArrayList();
+            strList.AddRange(new string[] { "1", "2", "3" });
+            Console.WriteLine(strList.Count);
+            strList.Add("4");
+            Console.WriteLine(strList.Count);
+            foreach (var item in strList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
